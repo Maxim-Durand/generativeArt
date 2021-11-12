498877f6a795644f0a00b5d64f7b2df8e96fae18
Maxim-Durand <durandmaxime1931@gmail.com>
2021-11-12 16:42:21 -0500

    Added orbitals

---
+++ orbitals/sketch.js
@@ -0,0 +1,56 @@
+let orbital
+let time = 0.05
+let all_orbitals = ["n1_l0_m0", "n2_l0_m0", "n2_l1_m0", "n2_l1_m1", "n2_l1_m-1",
+    "n3_l0_m0", "n3_l1_m0", "n3_l2_m0", "n3_l2_m2", "n3_l2_m-1", "n4_l2_m0", "n4_l2_m2",
+    "n4_l3_m0", "n4_l3_m2", "n4_l3_m4", "n4_l3_m-1", "n4_l3_m-2", "n4_l3_m-3",
+    "n5_l1_m0", "n5_l2_m0", "n5_l2_m2", "n5_l3_m0", "n5_l3_m1", "n5_l3_m2", "n5_l3_m3",
+    "n5_l3_m-2", "n5_l4_m0", "n5_l4_m1", "n5_l4_m2", "n5_l4_m3", "n5_l4_m4",
+    "n6_l3_m0","n6_l3_m1","n6_l3_m2","n6_l3_m3","n6_l4_m0","n6_l4_m1"]
+let selectedOrbital = "obj/h_orbita(" + all_orbitals[0] + ").obj"
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    background(200)
+    // put setup code here
+}
+
+function preload() {
+    fillSelectHTMLOption()
+    orbital = loadModel(selectedOrbital, true)
+}
+
+function fillSelectHTMLOption() {
+    let selector = document.getElementById("selector")
+    for (let orbital of all_orbitals) {
+        let opt = document.createElement('option');
+        opt.value = orbital;
+        opt.innerHTML = orbital;
+        selector.appendChild(opt);
+    }
+}
+
+function onSelect(selector) {
+    let value = selector.value
+    console.log(value)
+    selectedOrbital = "obj/h_orbita(" + value + ").obj"
+    orbital = loadModel(selectedOrbital, true)
+}
+
+function draw() {
+
+    time += 0.05
+    background(200)
+    ambientLight(200)
+    noStroke()
+    fill(200, 20, 20)
+    scale(2)
+    //rotate(0.1 * PI)
+    smooth()
+    //rotateX(frameCount * 0.01)
+    orbitControl()
+    rotateZ(frameCount * 0.01)
+    normalMaterial()
+    model(orbital)
+    // put drawing code here
+
+}
